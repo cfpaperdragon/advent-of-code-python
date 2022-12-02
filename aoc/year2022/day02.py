1aa104fa539ff4539f36dbae57e9dbe54e0a5930
Claudia Frutuoso <claudia.frutuoso@outsystems.com>
2022-12-02 15:35:30 +0000

    aoc-2022-02 avoid using IFs

--- aoc/year2022/day02.py
+++ aoc/year2022/day02.py
@@ -1,65 +1,55 @@
 # aoc.year2022.day02
 
-def calculate_score(input):
-    player1 = ['A', 'B', 'C']
-    player2 = ['X', 'Y', 'Z']
-    shape = {'Rock': 0, 'Paper': 1, 'Scissors': 2}
-    shape_scores = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
+def get_score_matrix():
+    """
+    return a score matrix where getting
+    score['Rock']['Paper'] = win
+    score['Rock']['Rock'] = draw
+    etc  
+    """
     lose = 0
     draw = 3
     win = 6
 
-    moves = input.strip().split(' ')
-    player1_move = moves[0]
-    player2_move = moves[1]
+    score = {}
+    score['Rock'] = {}
+    score['Rock']['Rock'] = draw
+    score['Rock']['Paper'] = win
+    score['Rock']['Scissors'] = lose
+    score['Paper'] = {}
+    score['Paper']['Rock'] = lose
+    score['Paper']['Paper'] = draw
+    score['Paper']['Scissors'] = win
+    score['Scissors'] = {}
+    score['Scissors']['Rock'] = win
+    score['Scissors']['Paper'] = lose
+    score['Scissors']['Scissors'] = draw
+    return score
+                        
+def get_reverse_matrix():
+    """
+    return the move to get a result
+    move['Rock']['X'] = 'Paper'
+    """
 
-    result = 0
-    shape_value = 0
-    # result for player 2
-    if player1_move == player1[shape['Rock']]:
-        if player2_move == player2[shape['Rock']]:
-            shape_value = shape_scores['Rock']
-            result = draw
-        elif player2_move == player2[shape['Paper']]:
-            shape_value = shape_scores['Paper']
-            result = win
-        elif player2_move == player2[shape['Scissors']]:
-            shape_value = shape_scores['Scissors']
-            result = lose
-        else:
-            raise Exception("Invalid value for player 2")
+    lose = 'X'
+    draw = 'Y'
+    win = 'Z'
 
-    elif player1_move == player1[shape['Paper']]:
-        if player2_move == player2[shape['Rock']]:
-            shape_value = shape_scores['Rock']
-            result = lose
-        elif player2_move == player2[shape['Paper']]:
-            shape_value = shape_scores['Paper']
-            result = draw
-        elif player2_move == player2[shape['Scissors']]:
-            shape_value = shape_scores['Scissors']
-            result = win
-        else:
-            raise Exception("Invalid value for player 2")
-
-    elif player1_move == player1[shape['Scissors']]:
-        if player2_move == player2[shape['Rock']]:
-            shape_value = shape_scores['Rock']
-            result = win
-        elif player2_move == player2[shape['Paper']]:
-            shape_value = shape_scores['Paper']
-            result = lose
-        elif player2_move == player2[shape['Scissors']]:
-            shape_value = shape_scores['Scissors']
-            result = draw
-        else:
-            raise Exception("Invalid value for player 2")
-
-    else:
-        raise Exception("Invalid value for player 1")
-
-    return shape_value + result
-                         
+    move = {}
+    move['Rock'] = {}
+    move['Rock'][draw] = 'Rock'
+    move['Rock'][win] = 'Paper'
+    move['Rock'][lose] = 'Scissors'
+    move['Paper'] = {}
+    move['Paper'][lose] = 'Rock'
+    move['Paper'][draw] = 'Paper'
+    move['Paper'][win] = 'Scissors'
+    move['Scissors'] = {}
+    move['Scissors'][win] = 'Rock'
+    move['Scissors'][lose] = 'Paper'
+    move['Scissors'][draw] = 'Scissors'
+    return move
 
 def calculate_score_part2(input):
     player1 = ['A', 'B', 'C']
@@ -127,7 +117,31 @@ def calculate(input_list, function_calculate_score):
 
 
 def calculate_part1(input_list):
-    return calculate(input_list, calculate_score)
+    player1 = {'A':'Rock', 'B':'Paper', 'C':'Scissors'}
+    player2 = {'X':'Rock', 'Y':'Paper', 'Z':'Scissors'}
+    shape_scores = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
+    score_matrix = get_score_matrix()
+
+    result = 0
+    for input in input_list:
+        moves = input.strip().split(' ')
+        player1_move = moves[0]
+        player2_move = moves[1]
+        result += score_matrix[player1[player1_move]][player2[player2_move]] + shape_scores[player2[player2_move]]
+    return result
+    
 
 def calculate_part2(input_list):
-    return calculate(input_list, calculate_score_part2)
+    player1 = {'A':'Rock', 'B':'Paper', 'C':'Scissors'}
+    shape_scores = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
+    score_matrix = get_score_matrix()
+    move_matrix = get_reverse_matrix()
+
+    result = 0
+    for input in input_list:
+        moves = input.strip().split(' ')
+        player1_move = moves[0]
+        match_result = moves[1]
+        player2_move = move_matrix[player1[player1_move]][match_result]
+        result += score_matrix[player1[player1_move]][player2_move] + shape_scores[player2_move]
+    return result
