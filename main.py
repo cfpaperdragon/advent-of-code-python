c20db88a9661059fc9f9b8850d4882938c9fd2a3
cfpaperdragon <claudia.frutuoso@gmail.com>
2023-08-18 10:42:15 +0100

    REFACTOR1 - refactor 1
    * moved notebooks to their own folder
    * refactor main.py to include docstring, exception handling and __main__

--- main.py
+++ main.py
@@ -1,45 +1,71 @@
-# main
+#!/usr/bin/env python3
+"""Call a particular problem of Advent of Code.
+
+Usage:
+
+   python3 main.py <year> <day> <part>
+"""
 import sys
 import time
 
 import aoc.common.process_input
 from aoc.common.import_utils import import_module
 
-n = len(sys.argv)
-if n != 4:
-    print("Missing parameters - needs 3 parameters: year day part")
-    exit(-1)
 
-year = sys.argv[1]
-day = sys.argv[2]
-part = sys.argv[3]
+def run_aoc(year, day, part):
+    """Run a specific problem of Advent of Code.
 
-module_name = "aoc.year" + year + ".day" + day 
-module = import_module(module_name)
+      Args:
+          year: the year of the problem 
+          day: the day of the problem (2 digits)
+          part: 1 or 2
 
-# record start time
-start = time.time()
+      Returns:
+          Result of executing the problem if implemented.
+    """
+    module_name = "aoc.year" + year + ".day" + day 
+    module = import_module(module_name)
 
-file_content = aoc.common.process_input.read_file("input\\year" + year + "\\day" + day + "\\input.txt")  
-process_function = int
-if year == "2021" and day == "22":
-    process_function = aoc.common.process_input.process_reboot_steps_line
-elif year == "2022":
-    process_function = str      
-if year == "2022" and day == "05":
-    input_content = aoc.common.process_input.to_str_list_no_strip(file_content)   
-else:     
-    input_content = aoc.common.process_input.to_function_list(file_content, process_function)
+    file_content = aoc.common.process_input.read_file("input\\year" + year + "\\day" + day + "\\input.txt")  
+    process_function = int
+    if year == "2021" and day == "22":
+        process_function = aoc.common.process_input.process_reboot_steps_line
+    elif year == "2022":
+        process_function = str      
+    if year == "2022" and day == "05":
+        input_content = aoc.common.process_input.to_str_list_no_strip(file_content)   
+    else:     
+        input_content = aoc.common.process_input.to_function_list(file_content, process_function)
 
-if part == "1":
-    if year == "2022" and day == "15":
-        result = module.calculate_part1(input_content, 2000000)
+    if part == "1":
+        if year == "2022" and day == "15":
+            result = module.calculate_part1(input_content, 2000000)
+        else:
+            result = module.calculate_part1(input_content)
     else:
-        result = module.calculate_part1(input_content)
-else:
-    result = module.calculate_part2(input_content)
+        result = module.calculate_part2(input_content)
 
-# record end time
-end = time.time()
+    return result
 
-print("Result:", result, "in", (end-start) * 10**3, "ms")
+
+if __name__ == '__main__':
+
+    try:
+        year = sys.argv[1]
+        day = sys.argv[2]
+        part = sys.argv[3]
+
+        # record start time
+        start = time.time()
+        result = run_aoc(year, day, part)
+        # record end time
+        end = time.time()
+        print("Result:", result, "in", (end-start) * 10**3, "ms")
+    except IndexError:
+        print("Missing parameter: needs to be called with 3 parameters: year day part")
+    
+    except ModuleNotFoundError as e: 
+        print(f"{e!r}", file=sys.stderr)
+
+    finally:
+        print("End.")
